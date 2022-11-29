f428ba44fcc9a3ef5dd49330405eaa0c64d1f34c
Ivan Svetov <ivan_lbov@mail.ru>
2022-11-29 23:19:05 +0500

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
 
-        System.out.println("Hello world!");
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        int[] array3 = new int[3];
+        array3[0] = 8;
+        array3[1] = 54;
+        array3[2] = 12;
+
+        System.out.println("Task 2");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i != array1.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]);
+            if (i != array2.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i]);
+            if (i != array3.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("");
+        System.out.println("Task 3");
+        for (int i = array1.length - 1; i >= 0 ; i--) {
+            System.out.print(array1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(" ");
+        for (int i = array2.length - 1; i >= 0 ; i--) {
+            System.out.print(array2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(" ");
+        for (int i = array3.length - 1; i >= 0 ; i--) {
+            System.out.print(array3[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("");
+        System.out.println("Task 4");
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] % 2 != 0) {
+                array1[i] += 1;
+            }
+
+        }
+        System.out.print(Arrays.toString(array1));
     }
 }
